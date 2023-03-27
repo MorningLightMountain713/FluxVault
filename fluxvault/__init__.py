f14b32ee397ccb169c693607c1b1f9b02671821b
David White <david.white@trustpower.co.nz>
2023-03-27 14:58:21 +1300

    Bump fluxrpc to fix up dropped socket error on server

--- fluxvault/__init__.py
+++ fluxvault/__init__.py
@@ -2,3 +2,4 @@ from fluxvault.extensions import FluxVaultExtensions
 from fluxvault.fluxagent import FluxAgent
 from fluxvault.fluxkeeper import FluxKeeper
 from fluxvault.fluxkeeper import FluxAppManager
+from fluxvault.helpers import AppMode
